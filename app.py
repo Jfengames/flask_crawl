16eda2c20ee606f4b753cb6b228ea0691d15f844
jiahui home <jiahui@home.com>
2018-06-07 07:19:59 -0700

    ...

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask,render_template,url_for
+import config
 
 app = Flask(__name__)
+app.config.from_object(config)
+
+@app.route('/login/')
+def login():
+    return render_template('login.html')
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/mission/')
+def mission():
+    return 'configure your crawler mission!'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
