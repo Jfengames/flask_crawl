097a944e16b8df247ca6b3767e32efa96535213a
feng wei <fengwei411@126.com>
2018-10-12 10:36:07 +0800

    add manage.py and workspace.xml

--- manage.py
+++ manage.py
@@ -4,35 +4,19 @@ Created on Wed Jun  6 15:44:20 2018
 
 @author: X1Carbon
 """
-
-from flask import Flask,session
 from flask_script import Manager
-from database import db,User
-import config
-from apis.userapi import user
-from apis.indexapi import index
-
-app = Flask(__name__)
-app.config.from_object(config)
+from flask_migrate import Migrate,MigrateCommand
+from main import app
+from database import db
 
 
-db.init_app(app)
-db.create_all(app=app)
-app.register_blueprint(blueprint=user)
-app.register_blueprint(blueprint=index)
-manager = Manager(app=app)
+manager = Manager(app)
 
-@app.context_processor
-def my_context_processor():
-    user_id = session.get('user_id')
-    if user_id:
-        user = User.query.filter(User.id == user_id).first()
+migrate = Migrate(app,db)
 
-        if user:
-            return {'user':user}
-    return {}
+manager.add_command('db',MigrateCommand)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     manager.run()
 
 
